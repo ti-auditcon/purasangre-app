fc362e0f34670473127ca382785bfd31b89fe0e6
dandelior <jberrioscifuentes@gmail.com>
2018-10-29 18:38:36 -0300

    alpha (2.0.1)

--- src/app/auth/reservas/reservas.page.ts
+++ src/app/auth/reservas/reservas.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Platform } from '@ionic/angular';
-// import { BottomButtonService } from '../../services/bottombutton.service';
+import { ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-reservas',
@@ -11,20 +11,32 @@ import { Platform } from '@ionic/angular';
 export class ReservasPage {
 
   buttonFix: string = "";
+  // alturaTabBar = document.querySelector('.clase-card');
+  // printAlturaBar = this.alturaTabBar.offsetHeight;
+  elemento = <HTMLElement>document.querySelector('.sc-ion-tabbar-md-h');
+  // altura = this.elemento.offsetHeight;
+
+  // boton = document.querySelector('.buttonFix');
+
   // constructor( private navCtrl: NavController ) {}
   // changeClassIOS = document.querySelectorAll('.button-fix');
 
+  // @ViewChild('.clase-card') elementView: ElementRef;
+  // viewHeight: number;
+
   constructor( private navCtrl: NavController, public plt: Platform ) {
+    // this.viewHeight = this.elementView.nativeElement.offsetHeight;
+
     if (this.plt.is('ios')) {
       // this.changeClassIOS.classList.add('button-fix-ios');
       // This will only print when on iOS
-      this.buttonFix = "button-fix button-fix-ios"
+      this.buttonFix = "button-fix button-fix-ios";
       // console.log('Este es un dispositivo iOS');
     } else {
-      this.buttonFix = "button-fix"
+      this.buttonFix = "button-fix";
+      // console.log(this.altura);
     }
   }
-
 
   goToEditHour() {
     this.navCtrl.navigateForward( '/home/(reservas:edit-hour)' );
