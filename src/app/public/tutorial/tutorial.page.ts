01a7b9ad99bf90f190fb77870afb585fb41d0f5b
boryeira <jmanuel.jorquera@gmail.com>
2019-02-06 09:48:57 -0300

    tutorial storage bug

--- src/app/public/tutorial/tutorial.page.ts
+++ src/app/public/tutorial/tutorial.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tutorial',
@@ -9,11 +10,30 @@ import { Router } from '@angular/router';
 })
 export class TutorialPage {
 
-  constructor(private storage: Storage, private router: Router) {}
+  constructor(private storage: Storage,
+              private router: Router,
+              public toastController: ToastController) {}
 
-  async finish() {
-    await this.storage.set('tutorialComplete', true);
-    this.router.navigateByUrl('/');
-  }
+
+    async presentToast(text = 'Error', duration = 2500) {
+      const toast = await this.toastController.create({
+       message: text,
+       duration: duration,
+       position: 'top'
+      });
+      toast.present();
+    }
+
+    async finish() {
+      await this.storage.set('tutorialComplete', true)
+              .then(res => {
+                this.presentToast('Tutorial terminado');
+                this.router.navigateByUrl('/');
+              })
+              .catch(err => {
+                this.presentToast('Error:'+err);
+              });
+
+    }
 
 }
